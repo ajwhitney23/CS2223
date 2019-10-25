e378e2c21f6e335c034d63dbab73bf756cf379d8
ajwhitney23 <andrewwhitney689@gmail.com>
2019-10-25 12:24:55 -0400

    latest

--- hw1/main.cpp
+++ hw1/main.cpp
@@ -9,11 +9,11 @@ int main(int argc, char *argv[])
 {
     board b;
     b.printBoard();
-    assert(b.alterBoard(HEA,3));
+    //assert(b.alterBoard(HEA,3));
     b.printBoard();
-    assert(b.alterBoard(DIA,4));
+    //assert(b.alterBoard(DIA,4));
     b.printBoard();
-    assert(!b.alterBoard(SPA,7));
+    //assert(!b.alterBoard(SPA,7));
     b.printBoard();
 
     board bo;
