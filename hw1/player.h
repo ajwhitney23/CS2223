0c7e624d4ebeb9c3ced4b0e6cdab0c82a5a67f99
Andrew Whitney <48370831+ajwhitney23@users.noreply.github.com>
2019-10-31 10:17:30 -0400

    Update player.h

--- hw1/player.h
+++ hw1/player.h
@@ -1,12 +1,3 @@
-//h file for player
-
-/*
-zero - condition wins
-A XOR B XOR C == 0 <-- win
-function to xor numLeft(heart) numLeft(spade) numLeft(diamond)
-need to figure out what to do based on result
-
-*/
 
 class player
 {
