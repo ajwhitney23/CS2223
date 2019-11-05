1a68f5b8c331649a9d68f5509b6e29f643e9a74e
ajwhitney23 <andrewwhitney689@gmail.com>
2019-11-05 15:46:10 -0500

    added problem 4

---
+++ hw2/problem4.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <cstdlib>
+#include <time.h>
+
+using namespace std;
+int row1 [4] = {1, 14, 14, 4};
+int row2 [4] = {11, 7, 6, 9};
+int row3 [4] = {8 ,10, 10, 5};
+int row4 [4] = {13, 2, 3, 15};
+
