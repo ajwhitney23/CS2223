c2968b7fd04acaf2fa388cbbb5db53dc94af0deb
ajwhitney23 <andrewwhitney689@gmail.com>
2019-11-05 13:06:37 -0500

    p3

--- hw2/problem3.cpp
+++ hw2/problem3.cpp
@@ -6,9 +6,11 @@
  */
 #include <iostream>
 #include <cstdlib>
+#include <time.h>
 
 using namespace std;
 int printed;
+clock_t t;
 
 long int p2(int n)
 //need to check if >0 for inital n
@@ -18,6 +20,8 @@ long int p2(int n)
         if (n == printed)
         {
             cout << n << ": " << 2 << endl;
+            cout << "time: " << ((double)(clock() - t)/CLOCKS_PER_SEC) << endl;
+            cout << endl;
             printed++;
         }
         return 2;
@@ -27,6 +31,8 @@ long int p2(int n)
         if (n == printed)
         {
             cout << n << ": " << 1 << endl;
+            cout << "time: " << ((double)(clock() - t)/CLOCKS_PER_SEC) << endl;
+            cout << endl;
             printed++;
         }
         return 1;
@@ -37,6 +43,8 @@ long int p2(int n)
         if (n == printed)
         {
             cout << n << ": " << r << endl;
+            cout << "time: " << ((double)(clock() - t)/CLOCKS_PER_SEC) << endl;
+            cout << endl;
             printed++;
         }
 
@@ -46,16 +54,17 @@ long int p2(int n)
 
 int main(int argc, char *argv[])
 {
-
     if (argc != 2)
     {
-        cout << "./p2 [number to run]" << endl;
+        cout << "./p3 [number to run]" << endl;
         exit(1);
     }
     int max = atoi(argv[1]);
     printed = 0;
+    t = clock();
     int r = p2(max);
-    cout << max << ": " << r << endl;
+    cout << max - 1 << ": " << r << endl;
+    cout << "time: " << ((double)(clock() - t)/CLOCKS_PER_SEC) << endl;
     /*
     for (int i = 0; i < max; i++)
     {
