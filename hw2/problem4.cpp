8e4c48eafd1de4fcede37db3f6ef35769f7a3d91
afsimoneau <afsimoneau@wpi.edu>
2019-11-06 13:30:40 -0500

    2d array

--- hw2/problem4.cpp
+++ hw2/problem4.cpp
@@ -8,6 +8,7 @@ using namespace std;
 
 //global variables
 int grid[4][4] = {{1, 14, 14, 4}, {11, 7, 6, 9}, {8, 10, 10, 5}, {13, 2, 3, 15}};
+/*
 int sumRow1 = 0;
 int sumRow2 = 0;
 int sumRow3 = 0;
@@ -16,6 +17,7 @@ int sumCol1 = 0;
 int sumCol2 = 0;
 int sumCol3 = 0;
 int sumCol4 = 0;
+*/
 
 //compares if any array is equal to 33
 int is33(int array[4])
@@ -39,11 +41,12 @@ int main(int argc, int **argv)
     /*
     Initialize sums
     */
-    int i;
+
+    /*int i;
     int temp = 0;
     for (i = 0; i < 4; i++)
     {
-        sumRow1 = sumRow1 + row1[i];
+        sumRow1 = sumRow1 + grid[0][i];
         sumRow2 = sumRow2 + row2[i];
         sumRow3 = sumRow3 + row3[i];
         sumRow4 = sumRow4 + row4[i];
@@ -52,4 +55,5 @@ int main(int argc, int **argv)
     sumCol2 = row1[1] + row2[1] + row3[1] + row4[1];
     sumCol3 = row1[2] + row2[2] + row3[2] + row4[2];
     sumCol4 = row1[3] + row2[3] + row3[3] + row4[3];
+    */
 }
