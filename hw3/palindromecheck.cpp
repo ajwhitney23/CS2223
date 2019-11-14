1560e2e38df06f961f1610d001f1242cb5cd246f
Andrew Whitney <andrewwhitney689@gmail.com>
2019-11-14 09:25:50 -0500

    completed

--- hw3/palindromecheck.cpp
+++ hw3/palindromecheck.cpp
@@ -7,51 +7,55 @@ using namespace std;
 string stripInput(string input)
 {
     string punc = "! .,;:";
-    for_each(input.begin(), input.end(), [](char & c)
-    {
+    for_each(input.begin(), input.end(), [](char &c) {
         c = tolower(c);
     });
-    for(char x: punc) {
+    for (char x : punc)
+    {
         input.erase(remove(input.begin(), input.end(), x), input.end());
     }
 
-    cout << input << endl;
+    //cout << input << endl; //debug
     return input;
 }
 
 int isPalindrome(string input)
-{ 
+{
     int i = 0;
     string aString = stripInput(input);
-    for(i = 0; i < aString.length() - 1; i++);
+    for (i = 0; i < aString.length() - 1; i++)
+        ;
     {
-        if(aString.at(i) != aString.at(aString.length() - i - 1)){
+        if (aString.at(i) != aString.at(aString.length() - i - 1))
+        {
             return 0;
         }
-        else{
+        else
+        {
             return 1;
         }
     }
-    /*
-    check if beginning and end are equal
-    check if beginning + 1 and end - 1 are equal... contintue until done
-    */
 }
 
-
-
-
-int main(int agrc, char **argv)
+int main(int argc, char **argv)
 {
-    string input = argv[1];
-    if(isPalindrome(input))
+    if (argc == 2)
     {
-        cout << "It is a palindrome" << endl;
+        string input = argv[1];
+        if (isPalindrome(input))
+        {
+            cout << "It is a palindrome" << endl;
+        }
+        else
+        {
+            cout << "It is not a palindrome" << endl;
+        }
     }
-    else 
+    else
     {
-        cout << "It is not a palindrome" << endl;
+        cout << "Usage: ./palindrome \"Potential Palindrome\"" << endl;
+        cout << "Input must be within two \"s in order to work as intended" << endl;
     }
-    
+
     return 0;
 }
