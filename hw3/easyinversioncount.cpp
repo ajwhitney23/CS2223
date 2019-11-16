529d30e980b79baf32a5a7905ff6fde1b0fb2537
Andrew Whitney <andrewwhitney689@gmail.com>
2019-11-16 13:45:02 -0500

    sort almost working

--- hw3/easyinversioncount.cpp
+++ hw3/easyinversioncount.cpp
@@ -4,10 +4,61 @@
 #include <string>
 using namespace std;
 
+int initial[256];
 
+void bubblesort(int arrayLength)
+{
+    int i;
+    int j;
+    int swapped = 0;
+    int temp;
+    int temp2;
+    for (i = 1; i < arrayLength; i++)
+    {
+        swapped = 0;
+        for (j = 1; j < arrayLength; j++)
+        {
+            if (initial[j] > initial[j + 1])
+            {
+                temp = initial[j + 1];
+                temp2 = initial[j];
+                initial[j] = temp;
+                initial[j + 1] = temp2;
+                swapped = 1;
+            }
+        }
+        if(!swapped)
+        {
+            break;
+        }
+    }
+}
+
+void printArray(int array[], int arrayLength)
+{
+    int i;
+    for(i = 1; i < arrayLength; i++)
+    {
+        cout << array[i] << " ";
+    }
+    cout << endl;
+}
 
 
 int main(int argc, char **argv)
 {
+    int i;
+    int j;
+    for (j = 1; j < argc; j++)
+    {
+        initial[j] = atoi(argv[j]);
+    }
+    /*
+    initial[argc] = '/0';
+    sorted[argc] = '/0';
+    */
+    printArray(initial, argc);
+    bubblesort(argc);
+    printArray(initial, argc);
     
 }
