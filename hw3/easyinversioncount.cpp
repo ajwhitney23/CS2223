4eb777efa9180a0da0d3322e80d654269fea750b
afsimoneau <afsimoneau@wpi.edu>
2019-11-18 09:25:12 -0500

    fix easy i think? also added to make clean

--- hw3/easyinversioncount.cpp
+++ hw3/easyinversioncount.cpp
@@ -8,18 +8,27 @@ int standard[] = {10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
 
 int bubblesort(int array[], int length)
 {
-    int inv = 0;
+    int inv = 0, swap = 0;
     for (int i = 0; i < length; i++)
     {
-        for (int j = 0; j < length; j++)
+        for (int j = 0; j < length - 1; j++)
         {
-            if (array[j] > array[i])
+            if (array[j] > array[j + 1])
             {
-                int temp = array[i];
-                array[i] = array[j];
+                int temp = array[j + 1];
+                array[j + 1] = array[j];
                 array[j] = temp;
+                swap = 1;
                 inv++;
             }
+        }
+        if (swap != 0)
+        {
+            swap = 0;
+        }
+        else
+        {
+            break;
         }
     }
 
@@ -52,10 +61,10 @@ int main(int argc, char **argv)
         if (argc == 3)
         {
             cout << "Intial array: " << endl;
-            printArray(array, n - 1);
+            printArray(array, n);
             cout << "Inversions: " << bubblesort(array, n) << endl;
             cout << "Sorted array: " << endl;
-            printArray(array, n - 1);
+            printArray(array, n);
         }
         else
         {
