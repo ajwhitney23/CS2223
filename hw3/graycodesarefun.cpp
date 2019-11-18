cfb7d2b3a0f956963bf3726212565b961ac1ac4f
afsimoneau <afsimoneau@wpi.edu>
2019-11-18 13:13:40 -0500

    gray code. could do with revision?

--- hw3/graycodesarefun.cpp
+++ hw3/graycodesarefun.cpp
@@ -1,50 +1,74 @@
 #include <cstdlib>
 #include <iostream>
+#include <vector>
 using namespace std;
+
+void decToBinary(int n)
+{
+    // array to store binary number
+    int binaryNum[32];
+
+    // counter for binary array
+    int i = 0;
+    while (n > 0)
+    {
+
+        // storing remainder in binary array
+        binaryNum[i] = n % 2;
+        n = n / 2;
+        i++;
+    }
+
+    // printing binary array in reverse order
+    for (int j = i - 1; j >= 0; j--)
+        cout << binaryNum[j];
+}
 
 void printBin(int n)
 {
     int binary[32];
 
-    for (int i = 0; i < 32 && n > 0; i++, n = n / 2)
+    for (int i = 0; i < 32 && n > 0; i++)
     {
         binary[i] = n % 2;
+        n = n / 2;
     }
 
     for (int i = n - 1; i >= 0; i--)
+    {
         cout << binary[i];
+    }
 }
 
-int *BRGC(int n)
+vector<string> BRGC(int n)
 {
-    if (n == 1)
+    vector<string> a;
+    a.push_back("0");
+    a.push_back("1");
+
+    for (int i = 2; i < (1 << n); i = i << 1)
     {
-        int *ret = (int *)malloc((int)2);
-        ret[0] = 0;
-        ret[1] = 1;
-        return ret;
-    }
-    else
-    {
-        int *BR = (int *)BRGC(n - 1);            //next lowest order codes
-        int *L = (int *)malloc((int)(1 << (n))); //array to return
-        for (int i = 0; i < (1 << (n - 1)); i++)
+        for (int j = i - 1; j >= 0; j--)
         {
-            L[i] = BR[i];
-            L[(1 << n) - i] = BR[i];
-            L[(1 << n) - i] += (1 << n) ^ (1 << (n - 1)); //set end bit to 1, xor out the shifted bits that're set
+            a.push_back(a[j]);
         }
-        free(BR);
-        return L;
+        for (int j = 0; j < i; j++)
+        {
+            a[j] = "0" + a[j];         //add 0's
+            a[j + i] = "1" + a[j + i]; //add 1's
+        }
     }
+    return a;
 }
 
 //main function
 int main(int argc, char **argv)
 {
-    int *r = BRGC(4);
-    for (int i = 0; i < 16; i++)
+    vector<string> r = BRGC(atoi(argv[1]));
+    cout << r.size() << endl;
+    /*for (int i = 0; i < r.size(); i++)
     {
-        printBin(r[i]);
+        cout << r[i] << endl;
     }
+    */
 }
