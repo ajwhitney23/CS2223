ba43c41ab5fae2b6d0d60297d8c64ea63597e2ec
afsimoneau <afsimoneau@wpi.edu>
2019-11-18 14:21:26 -0500

    alice,bob,chris,dylan

--- hw3/graycodesarefun.cpp
+++ hw3/graycodesarefun.cpp
@@ -3,62 +3,60 @@
 #include <vector>
 using namespace std;
 
-void decToBinary(int n)
+void BRGCProf(vector<string> v)
 {
-    // array to store binary number
-    int binaryNum[32];
-
-    // counter for binary array
-    int i = 0;
-    while (n > 0)
+    for (int i = 1; i < v.size(); i++)
     {
-
-        // storing remainder in binary array
-        binaryNum[i] = n % 2;
-        n = n / 2;
-        i++;
-    }
-
-    // printing binary array in reverse order
-    for (int j = i - 1; j >= 0; j--)
-        cout << binaryNum[j];
-}
-
-void printBin(int n)
-{
-    int binary[32];
-
-    for (int i = 0; i < 32 && n > 0; i++)
-    {
-        binary[i] = n % 2;
-        n = n / 2;
-    }
-
-    for (int i = n - 1; i >= 0; i--)
-    {
-        cout << binary[i];
+        int a = stoi(v[i - 1], nullptr, 2);
+        int b = stoi(v[i], nullptr, 2);
+        int diff = a - b;
+        cout << v[i] << ": ";
+        if (abs(diff) == 1)
+        {
+            cout << "alice";
+        }
+        else if (abs(diff) == 2)
+        {
+            cout << "bob";
+        }
+        else if (abs(diff) == 4)
+        {
+            cout << "chris";
+        }
+        else if (abs(diff) == 8)
+        {
+            cout << "dylan";
+        }
+        if (diff < 0)
+        {
+            cout << " in" << endl;
+        }
+        else
+        {
+            cout << " out" << endl;
+        }
     }
 }
 
 vector<string> BRGC(int n)
 {
-    vector<string> a;
-    a.push_back("0");
-    a.push_back("1");
+    vector<string> v;
+    v.push_back("0");
+    v.push_back("1");
 
     for (int i = 2; i < (1 << n); i = i << 1)
     {
         for (int j = i - 1; j >= 0; j--)
         {
-            a.push_back(a[j]);
+            v.push_back(v[j]);
         }
         for (int j = 0; j < i; j++)
         {
-            a[j] = "0" + a[j];         //add 0's
-            a[j + i] = "1" + a[j + i]; //add 1's
+            v[j] = "0" + v[j];         //add 0's
+            v[j + i] = "1" + v[j + i]; //add 1's
         }
     }
-    return a;
+    return v;
 }
 
 //main function
@@ -69,6 +67,6 @@ int main(int argc, char **argv)
     /*for (int i = 0; i < r.size(); i++)
     {
         cout << r[i] << endl;
-    }
-    */
+    }*/
+    BRGCProf(r);
 }
