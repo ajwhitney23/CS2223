900e5edac7177aac9f57b4fa543f89259272d43d
Andrew Whitney <andrewwhitney689@gmail.com>
2019-11-24 10:33:05 -0500

    added test array

--- hw4/hw4.cpp
+++ hw4/hw4.cpp
@@ -2,8 +2,22 @@
 #include <iostream>
 using namespace std;
 
-int main(int argc, char **argv)
+int testArray[8][9] = {{1, 1, 1, 1, 1, 1, 1, 1, 0},
+                       {1, 2, 1, 1, 1, 1, 2, 1, 0},
+                       {1, 1, 3, 1, 1, 3, 1, 1, 0},
+                       {1, 1, 1, 4, 4, 1, 1, 1, 0},
+                       {11, 1, 1, 1, 1, 1, 1, 1, 20},
+                       {1, 1, 1, 1, -1, -1, -1, -1, 34},
+                       {1, 2, 3, 4, 5, 6, 7, 8, -51},
+                       {1, -1, 1, -1, 1, -1, 1, -1, -6}};
+
+void GDEM(int **Array)
 {
     
+}
+
+int main(int argc, char **argv)
+{
+
     return 0;
 }
