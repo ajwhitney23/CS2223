b2f5605dddebda2a7bd8aae3f92eb025f3d9a1b4
Andrew Whitney <andrewwhitney689@gmail.com>
2019-12-09 14:31:40 -0500

    added file

---
+++ hw6/queens.cpp
@@ -0,0 +1,22 @@
+#include <cstdlib>
+#include <iostream>
+using namespace std;
+
+bool isLegalPosition(int board[], int n)
+{
+    if(1)
+    {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+
+
+
+int main(int argc, char** argv)
+{
+    return 0;
+}
