530094c6b49c7ddb9883fc76c4b43f0d8c938385
Andrew Whitney <andrewwhitney689@gmail.com>
2019-12-09 18:37:22 -0500

    updated

---
+++ hw5/readme.txt
@@ -0,0 +1,23 @@
+---PROGRAM MUST BE RUN ON LINUX/MAC---
+
+---DIRECTIONS FOR USE---
+
+---COMPILE INSTRUCTIONS---
+open terminal in the directory this readme is located.
+
+Run 'make' to build all executables
+Run 'make clean' to delete the executables before rebuilding
+
+---RUNNING JUST THE PROGRAM---
+open terminal in the directory in which the executable files are located and
+run the following command.
+
+Usage for parts 1-3: ./p1
+
+Usage for part4: ./p4
+
+---NOTES---
+We were given an extension which is shown in extension.png
+
+We were unable to complete part4 so code implementing djikstra's algorithm from CS3516 was provided
+to show we understand the algorithm.
--- hw6/queens.cpp
+++ hw6/queens.cpp
@@ -6,9 +6,31 @@ using namespace std;
 
 int isTrue[BOARD_SIZE] = {3, 1, 4, 2};
 int isFalse[BOARD_SIZE] = {1, 2, 3, 4};
+int board[BOARD_SIZE][BOARD_SIZE];
 
-bool isLegalPosition(int array[], int n)
+
+void make2DArray(int array[])
 {
+    int array[BOARD_SIZE][BOARD_SIZE];
+    for(int j = 0; j < BOARD_SIZE; j++)
+    {
+        for(int k = 0; k < BOARD_SIZE; k++)
+        {
+            if((k+1) == array[j])
+            {
+                board[j][k] = 1;
+            }
+            else
+            {
+                board[j][k] = 0;   
+            }
+        }
+    }
+}
+
+bool isLegalPosition(int array[BOARD_SIZE][BOARD_SIZE], int n)
+{
+    
     return true;
 }
 
@@ -17,7 +39,8 @@ bool isLegalPosition(int array[], int n)
 
 int main(int argc, char** argv)
 {
-    int x = isLegalPosition(isTrue, BOARD_SIZE);
+
+    int x = isLegalPosition(board, BOARD_SIZE);
     if(x)
     {
         cout << "it is a legal position" << endl;
